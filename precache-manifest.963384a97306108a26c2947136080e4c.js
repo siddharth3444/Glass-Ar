18d5275304c5c90ecf2897ea997b8dd7ab133357
Siddharth <Siddharthgupta2451@gmail.com>
2025-02-15 14:56:21 +0530

    Updates

---
+++ precache-manifest.963384a97306108a26c2947136080e4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb30a84324f760bf4cfef08ccb2874d3",
+    "url": "/basic-virtual-tryon-glasses/index.html"
+  },
+  {
+    "revision": "eda6a0dbbd790e03fe11",
+    "url": "/basic-virtual-tryon-glasses/static/css/main.0d786a58.chunk.css"
+  },
+  {
+    "revision": "133d19927792430948a8",
+    "url": "/basic-virtual-tryon-glasses/static/js/2.0e41c9bf.chunk.js"
+  },
+  {
+    "revision": "9c5df5d5dce2829e03b2bb53c3f771ea",
+    "url": "/basic-virtual-tryon-glasses/static/js/2.0e41c9bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda6a0dbbd790e03fe11",
+    "url": "/basic-virtual-tryon-glasses/static/js/main.54d93961.chunk.js"
+  },
+  {
+    "revision": "1bb59851f499a2130e71",
+    "url": "/basic-virtual-tryon-glasses/static/js/runtime-main.1aa7a7ae.js"
+  }
+]);
